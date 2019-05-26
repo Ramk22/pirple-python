45010b0761c0ff817d32f7ca28cf4bf29eee335d
ram_22 <ramakumar.ja@in.pwc.com>
2019-05-26 12:23:23 +0530

    variables homework

---
+++ variables/variable_hw.py
@@ -0,0 +1,23 @@
+"""
+My favourite song is the Bohemian Raphsody by Queen
+"""
+# String variables
+SongName = "Bohemian Raphsody"
+print("Song Name: " + SongName)
+Artist = "Queen"
+print("Artist:" +Artist)
+Album = "A Night at the Opera"
+print("Album:" + Album)
+Genres = "Classic Rock" + "Rock"
+print("Genres:" + Genres)
+
+# Integer variables
+ReleaseDate = 1975
+print("Release Date:")
+print(ReleaseDate)
+
+# Float Variables
+SongRunTime = 6.06
+
+print("Song Run Time:")
+print(SongRunTime)
